78e43911c14c55812152d37e1f5af999284d7cd8
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-03-02 17:27:16 -0500

    feat: Initial configuration of Docker, Laravel and Vite infrastructure

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<h1 class="text-3xl font-bold underline text-blue-600 mt-10 text-center">
+    ¡Hola mundo, GYMADM está vivo!
+</h1>
+</body>
+</html>
