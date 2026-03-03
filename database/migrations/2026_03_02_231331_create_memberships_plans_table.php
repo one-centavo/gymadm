20dfb6d54780cac1db9aa6b851b9b7067330a822
Gustavo Cabrera <gustavoadolfocabrera15@gmail.com>
2026-03-03 06:19:08 -0500

    feat: I have added the system migrations (#2)

---
+++ database/migrations/2026_03_02_231331_create_memberships_plans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memberships_plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50)->unique();
+            $table->string('description',255)->nullable();
+            $table->decimal('price',10,2);
+            $table->integer('duration_value');
+            $table->integer('duration_unit');
+            $table->string('status')->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memberships_plans');
+    }
+};
