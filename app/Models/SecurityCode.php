85cac6d2cd8238b811a02bdbb9ab890ed6f348e5
Gustavo Cabrera <gustavoadolfocabrera15@gmail.com>
2026-03-07 10:07:49 -0500

    Dev (#8)
    * Feature/creation seeders and factories (#4)
    
    * feat: I have created the factories
    
    * feat: I have corrected the MembershipFactory
    
    * feat: I have created the seeder correctly
    
    * fix: implement automated database seeding for members and plans
    
    * feat: I have created the OtpService with its logic and a temporal template for testing (#7)

--- app/Models/SecurityCode.php
+++ app/Models/SecurityCode.php
@@ -1,14 +1,35 @@
 <?php
 
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+use App\Models\User;
 
+/**
+ * @property int $id
+ * @property string $email
+ * @property string $code
+ * @property Carbon $expires_at
+ * @property Carbon|null $used_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode whereExpiresAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|SecurityCode whereUsedAt($value)
+ * @mixin \Eloquent
+ */
 class SecurityCode extends Model
 {
-    use HasFactory;
+
 
     protected $table = 'security_codes';
 
