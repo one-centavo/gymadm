1907d0da51838078f22e7e416f06f28ad6a5f863
Gustavo Cabrera <gustavoadolfocabrera15@gmail.com>
2026-03-07 10:07:20 -0500

    feat: I have created the OtpService with its logic and a temporal template for testing (#7)

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -6,6 +6,39 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property int $membership_plan_id
+ * @property \Illuminate\Support\Carbon $start_date
+ * @property \Illuminate\Support\Carbon $end_date
+ * @property string $status
+ * @property string $payment_method
+ * @property string|null $cancellation_reason
+ * @property \Illuminate\Support\Carbon|null $cancelled_at
+ * @property numeric $price_paid
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\MembershipPlan $membershipPlan
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\MembershipFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereCancellationReason($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereCancelledAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereEndDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereMembershipPlanId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership wherePaymentMethod($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership wherePricePaid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereStartDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Membership whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Membership extends Model
 {
     use HasFactory;
---
+++ resources/views/emails/security-code.blade.php
@@ -0,0 +1,3 @@
+<h1>Hola desde GYMADM</h1>
+<p>Tu código de seguridad es: <strong>{{ $otpCode }}</strong></p>
+<p>Este código expirará en 10 minutos.</p>
