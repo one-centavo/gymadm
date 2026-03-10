456e51897fcc25488148b80ee41d7b5527e97158
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-03-10 17:30:49 -0500

    feat: migrate registration to livewire and setup base layout with tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\Auth\RegisterForm;
+use App\Http\Controllers\Auth\LoginController;
+use App\Livewire\Dashboard\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +11,6 @@ Route::get('/', function () {
 
 
 Route::get('/register', RegisterForm::class)->name('register');
-Route::get('/login', function () {
-    return 'Página de Login en construcción';
-})->name('login');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
+Route::get('dashboard', Index::class)->name('dashboard');
