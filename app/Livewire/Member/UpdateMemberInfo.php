730399a67b3caf0108b324cc0d68f95c9276dcde
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-03-24 06:51:46 -0500

    feat: implement UpdateMemberInfor Livewire component

---
+++ app/Livewire/Member/UpdateMemberInfo.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Livewire\Member;
+
+use App\Services\MemberService;
+use App\Http\Requests\UpdateMemberInfoRequest;
+use Livewire\Component;
+use Livewire\Attributes\On;
+
+class UpdateMemberInfo extends Component
+{
+    public bool $open = false;
+
+    public string $memberId = '';
+    public string $first_name = '';
+    public ?string $middle_name = '';
+    public string $last_name = '';
+    public ?string $second_lastname = '';
+    public string $document_type = '';
+    public string $document_number = '';
+    public string $phone_number = '';
+    public string $email = '';
+
+    protected MemberService $memberService;
+
+    public function boot(MemberService $service)
+    {
+        $this->memberService = $service;
+    }
+
+    #[On('open-edit-drawer')]
+    public function loadMemberData($id)
+    {
+        $member = $this->memberService->getMemberById($id);
+        $this->fill($member->toArray());
+        $this->memberId = $id;
+        $this->open = true;
+    }
+
+    public function update()
+    {
+
+        $request = new UpdateMemberInfoRequest();
+        $validatedData = $this->validate(
+            $request->rules((int)$this->memberId),
+            $request->messages(),
+            $request->attributes()
+        );
+
+        $this->memberService->updateMemberInfo($this->memberId,$this->all());
+        $this->dispatch('member-updated');
+        $this->open = false;
+
+        session()->flash('message','Miembro actualizado correctamente.');
+    }
+}
