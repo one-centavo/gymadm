bb87db4791bf0a918ef718ae983753264930eba0
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-04-13 16:43:23 -0500

    feat: create the component and view for the admin history

---
+++ resources/views/livewire/admin/history/index.blade.php
@@ -0,0 +1,8 @@
+<section class="flex flex-col gap-6 pb-24 md:pb-0">
+    <header class="flex flex-col md:flex-row justify-between items-start md:items-center shrink-0">
+        <div>
+            <h1 class="text-4xl font-black tracking-tighter uppercase">HISTORIAL</h1>
+            <span class="text-slate-500 font-bold text-sm tracking-widest">GESTIONA Y CONTROLA EL HISTORIAL DE TU GIMNASIO</span>
+        </div>
+    </header>
+</section>
