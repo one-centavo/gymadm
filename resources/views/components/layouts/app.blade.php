4dc08a16c63e6480e77b2e987fbbf96703253170
Gustavo Cabrera <gustavoadolfocabrera15@gmail.com>
2026-07-03 23:29:19 -0500

    Feature/create config deploy (#79)
    * chore(docker): update docker configuration files
    
    * feat(ci): add deployment workflow
    
    * feat: actualizar configuración de despliegue, cambiar favicon a SVG y montar directorio public en docker-compose

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,36 +1,40 @@
 <!doctype html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <title>{{ config('app.name', 'GYMADM') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
+
 <body class="h-dvh flex flex-col">
 
-<header class="w-full shrink-0">
-    <x-partials.header/>
-</header>
+    <header class="w-full shrink-0">
+        <x-partials.header />
+    </header>
 
-<div class="flex flex-1 overflow-hidden">
+    <div class="flex flex-1 overflow-hidden">
 
-    <nav class="hidden md:flex md:flex-col md:w-1/5 overflow-y-auto bg-slate-900 border-r-4 border-black">
-        <x-partials.navbar/>
-    </nav>
+        <nav class="hidden md:flex md:flex-col md:w-1/5 overflow-y-auto bg-slate-900 border-r-4 border-black">
+            <x-partials.navbar />
+        </nav>
 
-    <main class="flex-1 overflow-y-auto p-4 md:p-8">
-        {{ $slot }}
-    </main>
+        <main class="flex-1 overflow-y-auto p-4 md:p-8">
+            {{ $slot }}
+        </main>
 
-</div>
+    </div>
 
-<footer class="md:hidden shrink-0 w-full z-50">
-    <nav>
-        <x-partials.navbar/>
-    </nav>
-</footer>
+    <footer class="md:hidden shrink-0 w-full z-50">
+        <nav>
+            <x-partials.navbar />
+        </nav>
+    </footer>
 
-@livewireScripts
+    @livewireScripts
 </body>
+
 </html>
