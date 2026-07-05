ae2f44773638d6277866dae743085e10f520b83f
one-centavo <gustavoadolfocabrera15@gmail.com>
2026-07-05 14:14:06 -0500

    bugfix: configure CSRF middleware to handle Livewire requests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,8 @@ use App\Http\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'ensure.registration.complete' => \App\Http\Middleware\EnsureRegistrationIsReady::class,
             'role' => RoleMiddleware::class
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
